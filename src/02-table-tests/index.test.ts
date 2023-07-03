40c85dba7b8a44fc93db1f3cdee8e326b2781fcf
Arina2246 <arinanebylova@gmail.com>
2023-07-03 15:36:43 +0200

    feat: add invalid actions to table tests

--- src/02-table-tests/index.test.ts
+++ src/02-table-tests/index.test.ts
@@ -3,27 +3,16 @@ import { simpleCalculator, Action } from './index';
 
 const testCases = [
   { a: 1, b: 2, action: Action.Add, expected: 3 },
-  { a: 2, b: 2, action: Action.Add, expected: 4 },
-  { a: 3, b: 2, action: Action.Add, expected: 5 },
   { a: 1, b: 2, action: Action.Subtract, expected: -1 },
-  { a: 2, b: 2, action: Action.Subtract, expected: 0 },
-  { a: 3, b: 2, action: Action.Subtract, expected: 1 },
   { a: 1, b: 2, action: Action.Multiply, expected: 2 },
-  { a: 2, b: 2, action: Action.Multiply, expected: 4 },
-  { a: 3, b: 2, action: Action.Multiply, expected: 6 },
   { a: 4, b: 2, action: Action.Divide, expected: 2 },
-  { a: 9, b: 3, action: Action.Divide, expected: 3 },
-  { a: 15, b: 5, action: Action.Divide, expected: 3 },
   { a: 1, b: 2, action: Action.Exponentiate, expected: 1 },
-  { a: 2, b: 2, action: Action.Exponentiate, expected: 4 },
-  { a: 3, b: 2, action: Action.Exponentiate, expected: 9 },
+  { a: 4, b: 2, action: 'invalid action', expected: null },
+  { a: 'aa', b: 'bb', action: Action.Exponentiate, expected: null },
 ];
 
 describe('simpleCalculator', () => {
-  test.each(testCases)(
-    'test Simple Calculator',
-    ({ a, b, action, expected }) => {
-      expect(simpleCalculator({ a, b, action })).toEqual(expected);
-    },
-  );
+  test.each(testCases)('table test', ({ a, b, action, expected }) => {
+    expect(simpleCalculator({ a, b, action })).toEqual(expected);
+  });
 });
